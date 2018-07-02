680757e967a63edaa2b6417837b1c7262a819769
Florian Schmitt <mrflos@gmail.com>
2018-07-02 17:26:46 +0200

    ajout initial

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# yeswiki-extension-googledrive
-Create google docs with bazar
+# Extension YesWiki googledrive
+### Champs pour formulaire bazar pour créer automatiquement un document collaboratif associé a une fiche
+
+
+En résumé :
+
+1. Créer un compte developpeur google a partir de votre compte google (voir https://developers.google.com en haut a droite) 
+2. Sur https://console.developers.google.com , créer un nouveau projet qui utilise l'API google drive (penser a activer l'api google drive quand ce sera proposé)
+3. Créer un identifiant "Créer une clé de compte de service" avec les infos suivantes : 
+  - Compte de service : App engine default service account
+  - Type de clé : P12 (Pour la rétrocompatibilité avec le code au format P12)
+  telecharger la clé et garder le mot de passe
+4. Aller sur la gestion des comptes de service
+5. Noter l'adresse du compte mail ('ID du compte de service'	'ID de clé') (on va devoir la mettre dans le fichier de conf.)
+6. copier la clé .p12 du compte service dans tools/googledrive/key.p12
+7. renseigner tools/googledrive/wiki.php ou dans wakka.config.php :
+$wakkaConfig['service_account_name'] = isset($wakkaConfig['service_account_name']) ? $wakkaConfig['service_account_name'] : "XXXXXXX@developer.gserviceaccount.com";
+8. créer un dossier sur son google drive, et le partager en écriture avec le mail du compte service créé auparavant
+9. récupérer l'id du dossier partagé en cliquant dessus et en prenant la derniere partie de la barre d'adresse et renseigner tools/googledrive/wiki.php ou dans wakka.config.php :
+$wakkaConfig['folder_id'] = isset($wakkaConfig['folder_id']) ? $wakkaConfig['folder_id'] : "XXXXXXX";
+10. utiliser la ligne suivante pour le formulaire bazar:  
+`collaborative_doc***bf_type_doc***Type de document*** *** *** *** *** ***0***0***`
---
+++ libs/googledrive.inc.php
@@ -0,0 +1,186 @@
+<?php
+
+/** collaborative_doc() - Ajoute un lien vers un document collaboratif (google ou etherpad)
+ * @param    mixed   L'objet QuickForm du formulaire
+ * @param    mixed   Le tableau des valeurs des différentes option pour l'élément liste
+ * @param    string  Type d'action pour le formulaire : saisie, modification, vue,... saisie par défaut
+ */
+function collaborative_doc(&$formtemplate, $tableau_template, $mode, $valeurs_fiche)
+{
+    $valliste = $GLOBALS['wiki']->config['apps'];
+
+    if ($mode == 'saisie') {
+        if (isset($_GET['deletefile']) and $_GET['deletefile']== 'true' and ($GLOBALS['wiki']->UserIsAdmin() or $GLOBALS['wiki']->UserIsOwner())) {
+            //on efface une entrée de la base de données
+            unset($valeurs_fiche[$tableau_template[1].$valeurs_fiche[$tableau_template[1]].'_url']);
+            unset($valeurs_fiche[$tableau_template[1]]);
+            $valeur = $valeurs_fiche;
+            $valeur['date_maj_fiche'] = date('Y-m-d H:i:s', time());
+            $valeur['id_fiche'] = $valeurs_fiche['id_fiche'];
+            if (YW_CHARSET != 'UTF-8') {
+                $valeur = array_map('utf8_encode', $valeur);
+            }
+            $valeur = json_encode($valeur);
+
+            //on sauve les valeurs d'une fiche dans une PageWiki, pour garder l'historique
+            $GLOBALS["wiki"]->SavePage($valeurs_fiche['id_fiche'], $valeur);
+        }
+        // test de l'existance d'un fichier, dans ce cas on ne propose pas sa creation
+        $urldoc = '';
+        if (isset($valeurs_fiche[$tableau_template[1]]) && isset($valeurs_fiche[$tableau_template[1].$valeurs_fiche[$tableau_template[1]].'_url'])) {
+            $urldoc = $valeurs_fiche[$tableau_template[1].$valeurs_fiche[$tableau_template[1]].'_url'];
+        }
+
+        if (empty($urldoc)) {
+            $bulledaide = '';
+            if (isset($tableau_template[10]) && $tableau_template[10] != '') {
+                $bulledaide = ' &nbsp;&nbsp;<img class="tooltip_aide" title="'.
+                htmlentities($tableau_template[10], ENT_QUOTES, YW_CHARSET).
+                '" src="tools/bazar/presentation/images/aide.png" width="16" height="16" alt="image aide" />';
+            }
+
+            $select_html = '<div class="control-group form-group">'."\n".'<label class="control-label col-xs-3">'."\n";
+            if (isset($tableau_template[8]) && $tableau_template[8] == 1) {
+                $select_html .= '<span class="symbole_obligatoire">*&nbsp;</span>'."\n";
+            }
+            $select_html .= $tableau_template[2].$bulledaide.' : </label>'."\n".
+              '<div class="controls col-xs-8">'."\n";
+            $select_html .= '<select';
+            $select_attributes = ' class="form-control" id="'.$tableau_template[1].'" name="'.$tableau_template[1].'"';
+
+            if (isset($tableau_template[8]) && $tableau_template[8] == 1) {
+                $select_attributes .= ' required="required"';
+            }
+            $select_html .= $select_attributes.'>'."\n";
+
+            if (isset($valeurs_fiche[$tableau_template[1]]) && $valeurs_fiche[$tableau_template[1]] != '') {
+                $def = $valeurs_fiche[$tableau_template[1]];
+            } else {
+                $def = $tableau_template[5];
+            }
+
+            if ($def == '' && ($tableau_template[4] == '' || $tableau_template[4] <= 1) || $def === "0") {
+                $select_html .= '<option value="" selected="selected">Aucun</option>'."\n";
+            }
+
+            if (is_array($valliste)) {
+                foreach ($valliste as $key => $label) {
+                    $select_html .= '<option value="'.$key.'"';
+                    if ($def != '' && $key == $def) {
+                        $select_html .= ' selected="selected"';
+                    }
+                    $select_html .= '>'.$label.'</option>'."\n";
+                }
+            }
+            $select_html .= "</select>\n</div>\n</div>\n";
+        } else {
+            $select_html = '<div class="control-group form-group">'."\n".'<label class="control-label col-sm-3">'."\n";
+            $select_html .= 'Lien vers le document : </label>'."\n".
+              '<div class="controls col-sm-9">'."\n".'<a href="'.$urldoc.'">'.$urldoc.'</a>';
+            $select_html .= '<input type="hidden" value="'.$urldoc.'" name="'.
+                $tableau_template[1].$valeurs_fiche[$tableau_template[1]].'_url'.'">';
+            $select_html .= '<input type="hidden" value="'.$valeurs_fiche[$tableau_template[1]].'" name="'.
+                    $tableau_template[1].'">';
+            if ($GLOBALS['wiki']->UserIsAdmin() or $GLOBALS['wiki']->UserIsOwner()) {
+              $select_html .= ' <a class="btn btn-xs btn-danger" href="'.$GLOBALS['wiki']->href('edit', $valeurs_fiche['id_fiche'], 'deletefile=true').'"><i class="glyphicon glyphicon-trash"></i> Supprimer</a>'."\n";
+            }
+            $select_html .= "\n</div>\n</div>\n";
+        }
+        return $select_html;
+    } elseif ($mode == 'requete') {
+        //si le doc n'existe pas, on le crée
+        if (!empty($valeurs_fiche[$tableau_template[1]]) and !isset($valeurs_fiche[$tableau_template[1].$valeurs_fiche[$tableau_template[1]].'_url']) ||
+            $valeurs_fiche[$tableau_template[1].$valeurs_fiche[$tableau_template[1]].'_url'] == '') {
+            if ($valeurs_fiche[$tableau_template[1]] == 'etherpad') {
+                return array(
+                    $tableau_template[1] => $valeurs_fiche[$tableau_template[1]],
+                    $tableau_template[1].$valeurs_fiche[$tableau_template[1]].'_url' => $GLOBALS['wiki']->config['etherpad_url']
+                            .genere_nom_wiki($valeurs_fiche['bf_titre']),
+                );
+            } else {
+                // documents google
+                $client = new Google_Client();
+                $client->setApplicationName('Collaborative docs');
+                $service = new Google_Service_Drive($client);
+
+                if (isset($_SESSION['service_token'])) {
+                    $client->setAccessToken($_SESSION['service_token']);
+                }
+                $key = file_get_contents($GLOBALS['wiki']->config['key_file_location']);
+                $cred = new Google_Auth_AssertionCredentials(
+                    $GLOBALS['wiki']->config['service_account_name'],
+                    array('https://www.googleapis.com/auth/drive'),
+                    $key
+                );
+
+                $client->setAssertionCredentials($cred);
+                if ($client->getAuth()->isAccessTokenExpired()) {
+                    $client->getAuth()->refreshTokenWithAssertion($cred);
+                }
+
+                $_SESSION['service_token'] = $client->getAccessToken();
+
+                //Insert a file
+                $file = new Google_Service_Drive_DriveFile();
+                $file->setTitle($valeurs_fiche['bf_titre']);
+                if (isset($valeurs_fiche['bf_description']) && $valeurs_fiche['bf_description'] != '') {
+                    $file->setDescription($valeurs_fiche['bf_description']);
+                }
+                if ($valeurs_fiche[$tableau_template[1]] == 'spreadsheet') {
+                    $file->setMimeType('application/vnd.google-apps.spreadsheet');
+                } elseif ($valeurs_fiche[$tableau_template[1]] == 'presentation') {
+                    $file->setMimeType('application/vnd.google-apps.presentation');
+                } else {
+                    $file->setMimeType('application/vnd.google-apps.document');
+                }
+
+                // dossier de destination
+                $parent = new Google_Service_Drive_ParentReference();
+                $parent->setId($GLOBALS['wiki']->config['folder_id']);
+                // cet id correspond à un dossier partage du drive
+                // (avec le $GLOBALS['wiki']->config['service_account_name'] en partage)
+                $file->setParents(array($parent));
+
+                $createdFile = $service->files->insert($file);
+
+                // droits d'acces
+                $newPermission = new Google_Service_Drive_Permission();
+
+                $newPermission->setValue('anyone');
+                $newPermission->setType('anyone');
+                $newPermission->setRole('writer');
+                $service->permissions->insert($createdFile['id'], $newPermission);
+
+                return array(
+                    $tableau_template[1] => $valeurs_fiche[$tableau_template[1]],
+                    $tableau_template[1].$valeurs_fiche[$tableau_template[1]].'_url' => $createdFile['alternateLink'],
+                );
+            }
+        } else {
+            return array(
+                $tableau_template[1] => $valeurs_fiche[$tableau_template[1]],
+                $tableau_template[1].$valeurs_fiche[$tableau_template[1]].'_url' => $valeurs_fiche[$tableau_template[1].$valeurs_fiche[$tableau_template[1]].'_url'],
+            );
+        }
+    } elseif ($mode == 'html') {
+        $html = '';
+        if (isset($valeurs_fiche[$tableau_template[1]]) && $valeurs_fiche[$tableau_template[1]] != '') {
+            $html .= '<div class="BAZ_rubrique" data-id="'.$tableau_template[1].'">'."\n".
+                '<span class="BAZ_label">'.$tableau_template[2].'&nbsp;:</span>'."\n".
+                '<span class="BAZ_texte">'."\n".$valliste[$valeurs_fiche[$tableau_template[1]]]."\n".'</a></span>'."\n".
+                '</div> <!-- /.BAZ_rubrique -->'."\n";
+        }
+        if (isset($valeurs_fiche[$tableau_template[1]]) and isset($valeurs_fiche[$tableau_template[1].$valeurs_fiche[$tableau_template[1]].'_url']) &&
+            $valeurs_fiche[$tableau_template[1].$valeurs_fiche[$tableau_template[1]].'_url'] != '') {
+            $html .= '<div class="BAZ_rubrique" data-id="'.$tableau_template[1].'_url'.'">'."\n".
+                '<span class="BAZ_label">Lien vers le document&nbsp;:</span>'."\n".
+                '<span class="BAZ_texte">
+                <a href="'.$valeurs_fiche[$tableau_template[1].$valeurs_fiche[$tableau_template[1]].'_url'].
+                '" target="_blank">'."\n".
+                $valeurs_fiche[$tableau_template[1].$valeurs_fiche[$tableau_template[1]].'_url']."\n".
+                '</a></span>'."\n".'</div> <!-- /.BAZ_rubrique -->'."\n";
+        }
+
+        return $html;
+    }
+}
